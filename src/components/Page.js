a3c63bd19ea88acda64fde4658432385def4a0a8
Yoanna Atanasova <atanasova.yoanna@gmail.com>
2021-08-25 20:13:57 +0300

    Create page wrapper component

---
+++ src/components/Page.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Page = styled.div`
+   padding: 20px 200px;
+
+    @media screen and (max-width: 960px){
+        padding: 20px 50px;
+    }
+`;
+
+export default Page
