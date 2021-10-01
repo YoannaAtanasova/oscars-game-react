153d10c10ac3dcee59daeb943e999e524da52688
Yoanna Atanasova <atanasova.yoanna@gmail.com>
2021-10-01 16:57:31 +0300

    Fix custom font

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -4,6 +4,6 @@ import FuturaLight from './assets/FuturaLight.ttf'
 export const GlobalStyle = createGlobalStyle`
     @font-face {
         font-family: 'Futura Light';
-        src: local('Futura Light'), local('FuturaLight') url(${FuturaLight}) format('truetype');
+        src: local('FuturaLight'), url(${FuturaLight}) format('truetype');
     }
 `;
