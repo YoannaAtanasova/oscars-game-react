6616b3db24d5fe405ba449505c9cd3619272f559
Yoanna Atanasova <atanasova.yoanna@gmail.com>
2021-10-07 17:57:22 +0300

    Load movies data from API call

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import MovieCard from '../components/MovieCard';
 import { Page, PageBody } from '../components/PageElements';
 
 function Movies() {
+    const [moviesData, setMoviesData] = useState([]);
+
+    useEffect(() => {
+        getMoviesData();
+    }, [])
+
+    const getMoviesData = async () => {
+        return await fetch("http://localhost:3030/movies-data")
+        .then((response) => response.json())
+            .then((data) => setMoviesData(data));
+      };
+
     return (
         <Page>
             <PageBody>
                 <MovieCard/>
+                {moviesData.map(movie => (
+                    <p>{movie.Title}</p>
+                ))}
             </PageBody>
         </Page>
     )
