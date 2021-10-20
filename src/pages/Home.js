f69cf430396724af3a8359591cfb64fbef23a0e4
Yoanna Atanasova <atanasova.yoanna@gmail.com>
2021-10-20 16:01:29 +0300

    Spice up the home page text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,17 +18,28 @@ function Home() {
             <Page>
                 <PageBody>
                     <Paragraph>
-                        You have reached the Proxiad Oscars Game website.
+                        Of all the Oscars joints in all the websites in all the world, you walk into mine.
+                    </Paragraph>
+
+                    <Paragraph>
+                        Toto, I've a feeling we're not in Kansas anymore, because this is the Proxiad Oscars Game website.
                     </Paragraph>
                     
                     <Paragraph>
                         Every Proxiad member can participate in the game by logging in with their existing Proxiad office 365 account.
+                        If you don't have one, frankly, my dear, I don't give a damn.
                     </Paragraph>
                     
+                    <Paragraph>You've got to ask yourself one question: 'Do I feel lucky?' Well, do ya, punk? Bucause I'm gonna make you an offer you can't refuse.</Paragraph>
+
                     <Paragraph>
                         You have the chance to show off your incredible prediction skills by guessing the Winners of this years Oscars nomenees.
+                        The rules are elementary, my dear Watson.
                         Take a look of all the Categories and Nomination.
-                        Make sure to take your picks before the start of the 93nd Academy Awards ceremony on April 25, 2021.
+                    </Paragraph>
+
+                    <Paragraph>
+                        'You talkin' to me?' Yeah, you. Make sure to take your picks before the start of the 93rd Academy Awards ceremony on April 25, 2021.
                     </Paragraph>
 
                     <Paragraph>
@@ -36,12 +47,12 @@ function Home() {
                     </Paragraph>
 
                     <Paragraph>
-                        Be sure to mark all the nominated movies that you have watched as well (and no cheating!).
-                        If several users have the same number of guessed Winners, the user who has watched more of the nominated movies will have an advantage.
+                        Be sure to mark all the nominated movies that you have watched as well. And no cheating bacuase, Houston, we are going to have a problem.
+                        If several users have the same number of guessed Winners, don't worry. Nobody puts Baby in a corner. The user who has watched more of the nominated movies will have an advantage.
                     </Paragraph>  
 
                     <Paragraph>
-                        Good luck and happy watching!
+                        Grab your popcorn, round up the usual suspects, happy watching and may the force be with you!
                     </Paragraph>          
                 </PageBody>
             </Page>
