75ae96fc34656640e7c58829eab24c6bde4cac2d
Yoanna Atanasova <atanasova.yoanna@gmail.com>
2021-12-08 14:22:24 +0200

    Fix nomination caegory margin on small screens

--- src/components/styled/NominationCategoryElements.js
+++ src/components/styled/NominationCategoryElements.js
@@ -2,4 +2,8 @@ import styled from 'styled-components';
 
  export const CategoryWrapper = styled.div`
     margin: 30px;
+
+      @media (max-width: 768px) {
+         margin: 20px 5px;
+      }
  `;
